9e97ac86228d34cc601f7f2f61428c8cfba27496
cristopher manuel <brandomm641@gmail.com>
2026-04-12 03:29:54 -0400

    Parte I: Implementacion de conversion de temperatura (Fahrenheit a Celsius y Kelvin)

--- AnalisisMotorIndustrial/Program.cs
+++ AnalisisMotorIndustrial/Program.cs
@@ -4,7 +4,27 @@ class Program
 {
     static void Main()
     {
+        //  Analisis Termico y Electrico
         Console.WriteLine("=== PRACTICA Analisis de Motor Industrial ===");
         Console.WriteLine("Analisis Termico y Electrico del Motor\n");
+
+        // PARTE I: TEMPERATURA
+
+        // La temperatura en °F se construye con los ultimos 2 digitos de la matricula
+        // Matricula: 0667 → temperatura = 267 °F
+        double temperaturaF = 267;
+
+        // Conversion de Fahrenheit a Celsius
+        double temperaturaC = (temperaturaF - 32) * 5 / 9;
+
+        // Conversion de Celsius a Kelvin
+        double temperaturaK = temperaturaC + 273.15;
+
+        // Mostrar resultados con 2 decimales
+        Console.WriteLine("----- PARTE I: TEMPERATURA -----");
+        Console.WriteLine($"Temperatura en Fahrenheit: {temperaturaF:F2} °F");
+        Console.WriteLine($"Temperatura en Celsius: {temperaturaC:F2} °C");
+        Console.WriteLine($"Temperatura en Kelvin: {temperaturaK:F2} K");
+
     }
 }
