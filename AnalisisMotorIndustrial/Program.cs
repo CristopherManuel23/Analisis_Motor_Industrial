fa183565039bf5c56838aeb9b958c363b4e6be34
cristopher manuel <brandomm641@gmail.com>
2026-04-11 22:17:47 -0400

    Agregar archivos de proyecto.

---
+++ AnalisisMotorIndustrial/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== PRACTICA IND-421 ===");
+        Console.WriteLine("Analisis Termico y Electrico del Motor\n");
+    }
+}
