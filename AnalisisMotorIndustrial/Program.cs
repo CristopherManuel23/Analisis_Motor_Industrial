3ac2f0b37807e9a27aa9e602be9cc3022e58c058
cristopher manuel <brandomm641@gmail.com>
2026-04-11 22:21:30 -0400

    Inicio del Proyecto

--- AnalisisMotorIndustrial/Program.cs
+++ AnalisisMotorIndustrial/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("=== PRACTICA IND-421 ===");
+        Console.WriteLine("=== PRACTICA Analisis de Motor Industrial ===");
         Console.WriteLine("Analisis Termico y Electrico del Motor\n");
     }
 }
